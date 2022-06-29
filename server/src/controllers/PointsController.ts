61bb449f74426db7563b606d7562a94d543260a0
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-06-29 04:36:40 -0300

    Feature: subindo as funcionalidades básicas do Node.js

---
+++ server/src/controllers/PointsController.ts
@@ -0,0 +1,73 @@
+import { Request, Response } from 'express'
+import knex from '../database/connection'
+
+class PointsController {
+  async create(request: Request, response: Response) {
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body
+
+    const point = { image: "image-fake", name, email, whatsapp, latitude, longitude, city, uf }
+
+    const trx = await knex.transaction()
+  
+    try {
+      const [ insertedId ] = await trx("points").insert(point)
+  
+      const pointItems = items.map((item_id: number) => {
+        return {
+          item_id,
+          point_id: insertedId
+        }
+      })
+  
+      await trx("point_items").insert(pointItems)
+
+      await trx.commit()
+  
+      return response.json({ id: insertedId, ...point })
+    } catch (error) {
+     
+      return response.json({ message: error })
+    }
+  }
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params
+
+    try {
+      const point = await knex("points").where('id', id).first()
+
+      if (!point) throw Error("Não foi encontrado nenhum ponto")
+
+      const items = await knex("items").join("point_items", "items.id", "=", "point_items.item_id")
+
+      return response.json({ point, items })
+
+    } catch (error) {
+      return response.json({ message: error })
+    }
+  }
+
+  async index (request: Request, response: Response) {
+    const { city, uf, items } = request.query
+
+   const parsedItems = String(items).split(",").map(item => Number(item.trim()))
+
+   const points = await knex("points")
+        .join("point_items", "points.id", "=", "point_items.point_id")
+        .whereIn("point_items.item_id", parsedItems)
+        .where("city", String(city))
+        .where("uf", String(uf))
+        .distinct()
+        .select("points.*")
+
+    try {
+
+      return response.json({ points })
+
+    } catch (error) {
+      return response.json({ message: error })
+    }
+  }
+}
+
+export default new PointsController()
---
+++ server/src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+import itemsController from './controllers/ItemsController'
+import pointsController from './controllers/PointsController'
+
+const routes = Router()
+
+routes.get("/items", itemsController.index)
+routes.post("/points", pointsController.create)
+routes.get("/points/:id", pointsController.show)
+routes.get("/points", pointsController.index)
+
+export default routes 
